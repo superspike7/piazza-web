d48b1ae9b001996fae8031555a4ff09ec74b3b87
Spike Vinz Cruz <spikevinz@gmail.com>
2024-09-11 14:26:39 +0800

    complete signup form

---
+++ test/controllers/users_controller_test.rb
@@ -0,0 +1,46 @@
+require "test_helper"
+
+class UsersControllerTest < ActionDispatch::IntegrationTest
+  test "redirects to feed after successful sign up" do
+    get sign_up_path
+    assert_response :ok
+
+    assert_difference ["User.count", "Organization.count"], 1 do
+      post sign_up_path, params: {
+                      user: {
+                        name: "Dryxcy",
+                        email: "mamabing@gmail.com",
+                        password: "password",
+                      },
+                    }
+    end
+
+    assert_redirected_to root_path
+    follow_redirect!
+    assert_select ".notification.is-success",
+                  text: I18n.t(
+                    "users.create.welcome",
+                    name: "Dryxcy"
+                  )
+  end
+
+  test "renders errors if input data is invalid" do
+    get sign_up_path
+    assert_response :ok
+
+    assert_no_difference ["User.count", "Organization.count"] do
+      post sign_up_path, params: {
+                      user: {
+                        name: "",
+                        email: "mamabing",
+                        password: "small",
+                      },
+                    }
+    end
+
+    assert_response :unprocessable_entity
+    assert_select "p.is-danger",
+                  text: I18n.t
+    ("activerecord.errors.models.user.attributes.password.too_short")
+  end
+end
